3c8e38611c7b129b274cd092e62694c29aae09f1
JanEuster <>
2022-01-15 17:30:23 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components"
+const calcImg = require("./casio_fx-82de_plus.png")
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='draggable'/>
+      <img
+        src={calcImg}
+        alt="Calculator Graphic"
+      ></img>
     </div>
   );
 }
